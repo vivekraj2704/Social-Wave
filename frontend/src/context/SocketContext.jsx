3916ef408ffcc1f0f1aaa0977381b05227f4e3c4
vivekraj2704 <rajvivek2704@gmail.com>
2024-09-17 20:24:28 +0530

    final Changes

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 	const user = useRecoilValue(userAtom);
 
 	useEffect(() => {
-		const socket = io("/", {
+		const socket = io("https://social-wave-a0v0.onrender.com", {
 			query: {
 				userId: user?._id,
 			},
